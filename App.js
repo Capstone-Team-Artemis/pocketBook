dc6ad365ca95b3ee3c7995e425ae0921b5ccae32
Sandy Dai <sandydai98@gmail.com>
2021-03-23 18:00:08 -0400

    FE(app): comments out SocketsFrontEnd

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import SocketsFrontEnd from './test/SocketsFrontend';
+// import SocketsFrontEnd from './test/SocketsFrontend';
 //import your component
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <SocketsFrontEnd />
+      {/* <SocketsFrontEnd /> */}
       {/* you can put your component to test */}
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />

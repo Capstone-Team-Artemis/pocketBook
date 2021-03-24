982a8686d954752b5dcfee1a3a532bf2bb12efa6
Seonae <seonaebyeon1@gmail.com>
2021-03-24 16:31:58 -0400

    BE: create POST and PUT routes for events

--- server/api/events.js
+++ server/api/events.js
@@ -11,3 +11,32 @@ module.exports = router
 //         next (err)
 //     }
 // )}
+
+//POST api/events/:userId/createEvent
+//host key 
+
+router.post("/:userId/createEvent", async (req, res, next) => {
+    try {
+        // let eventTitle = req.body.eventTitle;
+        // let eventDescrition = req.body.eventDescription;
+        // let eventDate = req.body.eventDate;
+        // let eventTime = req.body.eventTime;
+        const newEvent = await Event.create(req.body)
+        res.status(201).send(newCampus)
+    } catch (error) {
+        next(error)
+    }
+})
+
+//PUT api/events/:userId/updateEvent/:eventId 
+//host key 
+router.put('/events/:userId/updateEvent/:eventId', async(req, res, next) => {
+    try {
+        const eventId = req.params.eventId
+        const event = await Event.findByPk(eventId)
+        const updatedEvent = await campus.update(req.body)
+        res.send(updatedEvent)
+    } catch (error) {
+        next(error)
+    }
+})

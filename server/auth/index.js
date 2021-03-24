2746c2e3fd1ce0cbabbca73b71e827228c3b315b
Sandy Dai <sandydai98@gmail.com>
2021-03-24 19:20:00 -0400

    addPOST route for login

---
+++ server/auth/index.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const { User } = require('../db/models');
+module.exports = router;
+
+// POST auth/login
+router.post('/login', async (req, res, next) => {
+  try {
+    // Find user using the inputted email
+    const user = await User.findOne({ where: { email: req.body.email } });
+    // If can't find user, do this:
+    if (!user) {
+      console.log('No such user found with:', req.body.email);
+      res.status(401).send('Wrong username and/or password');
+    } else if (!user.correctPassword(req.body.password)) {
+      console.log('Incorrect password for email:', req.body.email);
+      res.status(401).send('Wrong username and/or password');
+      // If can find user, login!
+    } else {
+      req.login(user, (err) => (err ? next(err) : res.json(user)));
+    }
+  } catch (err) {
+    next(err);
+  }
+});

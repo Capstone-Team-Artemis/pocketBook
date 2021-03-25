7cecd67667ae317ea5c48ca70edf9266b6be9b71
Seonae <seonaebyeon1@gmail.com>
2021-03-25 12:31:50 -0400

    FE(UserProfile): edit views

--- client/UserProfile.js
+++ client/UserProfile.js
@@ -1,12 +1,48 @@
 import React, { userState } from 'react'
-import { SafeAreaView } from 'react-native'
+import { SafeAreaView, ScrollView, View, Text, StyleSheet, StatusBar} from 'react-native'
 
 export default UserProfile = () => {
     return (
-        <SafeAreaView>
-            <Text>My Bookshelf</Text>
+        <SafeAreaView style ={styles.container}>
+            <ScrollView style={styles.scrollView}>
+                <View>
+                <Text>UserName</Text>
+                </View>
+
+                <View>
+                <Text>My Bookshelf</Text>
+                </View>
+
+                <View>
+                <Text>Currently Reading</Text>
+                <ScrollView horizontal={true}>
+                    {/* add book info */}
+                <View style={styles.bookList}>
+                    {/* map out book list here */}
+                </View>
+                </ScrollView>
+                </View>
+
+                <View>
+                <Text>To Read</Text>
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
 
-export default UserProfile
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        paddingTop: StatusBar.currentHeight
+    },
+    scrollView:{
+        backgroundColor: 'pink',
+        marginHorizontal:10,
+    },
+    bookList: {
+        flexDirection: 'row'
+    }
+})
+
+//export default UserProfile

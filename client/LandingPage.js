69c0504e97934b5bb68f5c0d93b139ce7b3f5601
Kahoua (Kay) XiongPachay <kayxxx2020@gmail.com>
2021-03-24 22:38:50 -0400

    FE: Adds notes

--- client/LandingPage.js
+++ client/LandingPage.js
@@ -9,11 +9,12 @@ import {
   SafeAreaView,
   ScrollView,
   Image,
+  TouchableOpacity,
 } from "react-native";
 import { Formik } from "formik";
 // import GOOGLE_API from "../secrets";
 import axios from "axios";
-import SingleBookView from "./SingleBookView";
+// import SingleBookView from "./SingleBookView";
 const GOOGLE_API = "AIzaSyCCv2Y7h0jPvMK1NF0y_nmI9V-4_lTXsWg";
 
 export default function LandingPage() {
@@ -21,11 +22,14 @@ export default function LandingPage() {
   const [result, setResult] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
   const [featureBook, setFeatureBook] = useState({});
+  //You can think of useEffect like componentDidMount
   useEffect(() => {
+    //makes an axios call to fetch all books with query of "book" & ordered by newest published books
     const getRandomBook = async () => {
       const { data } = await axios.get(
         "https://www.googleapis.com/books/v1/volumes",
         {
+          //breaking up params can help with entering in different inputs for keys below and easier to see the params
           params: {
             q: "book",
             key: GOOGLE_API,
@@ -33,10 +37,15 @@ export default function LandingPage() {
           },
         }
       );
+      //book gives us a random book from data from the axios call
       const book = data.items[Math.floor(Math.random() * data.items.length)];
+      //sets the chosen book into featureBook
       setFeatureBook(book);
     };
+    //invoke getRandomBook
     getRandomBook();
+    //useEffect takes in 2nd argument - an array of dependencies
+    //dependencies are values defined outside of useEffect that are being used inside useEffect
   }, [setFeatureBook]);
 
   function handleChange(bookTitle) {
@@ -76,12 +85,17 @@ export default function LandingPage() {
           onChangeText={handleChange}
         />
         <Button title='Submit' onPress={handleSubmit} />
+        {/* featureBook will only show up once the component mounts */}
         {featureBook.id && (
-          <Image
-            alt={featureBook.volumeInfo.title}
-            source={{ uri: featureBook.volumeInfo.imageLinks.thumbnail }}
-            style={{ width: 200, height: 300, margin: "auto" }}
-          />
+          <View>
+            <Text>Newly Published</Text>
+            <Image
+              alt={featureBook.volumeInfo.title}
+              source={{ uri: featureBook.volumeInfo.imageLinks.thumbnail }}
+              style={{ width: 200, height: 300, margin: "auto" }}
+            />
+            {/* <Text>{featureBook.volumeInfo.description}</Text> */}
+          </View>
         )}
         <View style={styles.centeredView}>
           <Modal
@@ -99,6 +113,7 @@ export default function LandingPage() {
                     return (
                       <View key={idx}>
                         {/* Render book cover image first, with styling */}
+                        {/* <TouchableOpacity onPress={() => }> */}
                         <Image
                           // put react navigation here
                           alt={book.volumeInfo.title}
@@ -107,6 +122,7 @@ export default function LandingPage() {
                             uri: book.volumeInfo.imageLinks.smallThumbnail,
                           }}
                         />
+                        {/* </TouchableOpacity> */}
                         {/* Render book title, authors array, rating */}
                         <Text>{book.volumeInfo.title}</Text>
                         <Text>{book.volumeInfo.authors}</Text>

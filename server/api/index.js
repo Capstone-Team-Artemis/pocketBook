1739ae1792ffe159c4f580361af13d79ad0fa0fa
Kahoua (Kay) XiongPachay <kayxxx2020@gmail.com>
2021-03-26 12:12:03 -0400

    BE: Deletes comments

--- server/api/index.js
+++ server/api/index.js
@@ -1,25 +1,17 @@
 //create router instance
 
-const router = require('express').Router();
+const router = require("express").Router();
 module.exports = router;
 //api/users
-<<<<<<< HEAD
-router.use('/users', require('./users'))
+router.use("/users", require("./users"));
 //api/books
-router.use('/books', require('./books'))
+router.use("/books", require("./books"));
 //api/events
-router.use('/events', require('./events'))
-=======
-router.use('/users', require('./users'));
-//api/books
-router.use('/books', require('./books'));
-//api/events
-router.use('/events', require('./events'));
->>>>>>> main
+router.use("/events", require("./events"));
 
 //handle 404
 router.use((req, res, next) => {
-  const error = new Error('page not found!');
+  const error = new Error("page not found!");
   error.status = 404;
   next(error);
 });

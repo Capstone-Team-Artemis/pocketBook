ca56918ce0429a3c07884e2194fc817649296e86
Seonae <seonaebyeon1@gmail.com>
2021-03-28 23:46:06 -0400

    FE(CreateEvent): comments back the navigation

--- client/CreateEvent.js
+++ client/CreateEvent.js
@@ -66,7 +66,7 @@ class CreateEvent extends Component {
           <TouchableOpacity
             style={styles.navbar}
             style={{ alignItems: 'flex-end', margin: 16 }}
-            onPress={this.props.navigation.openDrawer}
+            //onPress={this.props.navigation.openDrawer}
           >
             <Icon name="bars" size={24} color="#161924" />
           </TouchableOpacity>

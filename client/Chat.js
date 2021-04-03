9e17ce3d52b59b711b3c6b28cb75338e2c5f436b
Seonae <seonaebyeon1@gmail.com>
2021-04-03 19:43:23 -0400

    FE(Chat): adds userName and avatar

--- client/Chat.js
+++ client/Chat.js
@@ -16,21 +16,22 @@ class Chat extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      chatMessage: '',
+      //chatMessage: '',
       //discussion: [],
       messages: [],
     };
   }
   componentDidMount() {
+    // place ngrok or deployed link here! 
     this.socket = io('http://127.0.0.1:3000', {
       transports: ['websocket'],
       jsonp: false,
     });
 
     this.socket.connect();
-
+    let roomName = this.props.route.params.title
     //******send room info to backend socket
-    this.socket.emit('room', this.props.route.params.eventId);
+    this.socket.emit('room', roomName);
 
     const thisComponent = this;
     //connecting to the backend socket
@@ -39,43 +40,22 @@ class Chat extends React.Component {
     });
     //STEP3: receiving messages from the backend
     this.socket.on('messages', (message) => {
-      // console.log('MESSAGES IN SOCKET MESSAGE -->', message);
-      // console.log(
-      //   'THISCOMPONENT.STATE.MESSAGES -->',
-      //   thisComponent.state.messages
-      // );
+
       const messages = thisComponent.state.messages.slice();
       //adding new message recived from the backend to the state
       thisComponent.setState({ messages: [message, ...messages] });
     });
   }
 
-  //********ROOMS
-  // componentWillUnmount() {
-  //   socket.emit('leaveChat', this.props.eventId)
-  // }
-
-  // onSend = ((messages = []) => {
-  //     setMessages((previousMessages) =>
-  //       GiftedChat.append(previousMessages, messages)
-  //     );
-  //     socket.emit('chat message', messages);
-  //   },
-  //   []);
-
   submitChatMessage(message) {
     //step1: socket is emitting chat message to the backend line6 of index.js
-    let eventId = this.props.route.params.eventId
-    let submitedMessage = message[0]
-    let addRoom = {...submitedMessage, eventId}
+    let eventTitle = this.props.route.params.title
+    let submittedMessage = message[0]
+    let addRoom = {...submittedMessage, eventTitle}
 
     let newMessage = []
     newMessage.push(addRoom)
     this.socket.emit('chat message', newMessage);
-
-    //*************** */
-    // let { user } = message
-    // console.log("MESSAGE****", user)
 
     this.setState((previousMessages) =>
       GiftedChat.append(previousMessages, message)
@@ -102,34 +82,29 @@ class Chat extends React.Component {
 
   render() {
     console.log('USER ID?? -->', this.props.userId);
-    console.log("username? ==>", this.props.userName)
-    console.log("userimage? ==>", this.props.image)
     console.log("eventId?? ==>", this.props.route.params.eventId)
     return (
       <GiftedChat
         messages={this.state.messages}
-        // onSend={(messages) => onSend(messages)}
         onSend={(message) => this.submitChatMessage(message)}
-        // user={{
-        //   _id: 1,
-        // }}
+
         user={{ 
           _id: this.props.userId,
           name: this.props.userName,
           avatar: this.props.image,
-          placeholder='Type your message here...',
          }}
-        // renderBubble={renderBubble}
+        renderBubble={this.renderBubble}
         showUserAvatar
         alwaysShowSend
         scrollToBottom
+        renderUsernameOnMessage={true}
       />
     );
   }
 }
 
 const mapState = (state) => {
-  //console.log("STATE***", state)
+  console.log("STATE***", state)
   return {
     userId: state.user.id,
     userName: state.user.username,

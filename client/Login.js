94bcac96e084cc2d439355f786cd0e0652d015f0
Sandy Dai <sandydai98@gmail.com>
2021-04-04 17:57:34 -0400

    FE(Login): style textinput

--- client/Login.js
+++ client/Login.js
@@ -7,6 +7,7 @@ import {
   ScrollView,
   TouchableOpacity,
   SafeAreaView,
+  ImageBackground,
   Alert,
 } from 'react-native';
 import { TextInput } from 'react-native-paper';
@@ -41,142 +42,150 @@ const Login = (props) => {
   }
 
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <Formik
-          initialValues={{
-            email: '',
-            password: '',
-          }}
-          onSubmit={async (values) => {
-            // If user didn't input anything:
-            if (values.email === '' || values.password === '') {
-              Alert.alert('Error', 'Username and/or password cannot be empty.');
-            } else {
-              // If user did, axios call to lookup user login info
-              try {
-                let res = await axios.post(
-                  'https://pocketbook-gh.herokuapp.com/auth/login/',
-                  {
-                    email: values.email,
-                    password: values.password,
-                  }
-                );
-                // handlePress = passes user info to function that will pass to RootNavigation's logIn function
-                handlePress({ user: res.data });
-                // If user info is invalid:
-              } catch {
-                Alert.alert(
-                  'Error',
-                  'Incorrect username or password. Please try again.'
-                );
-              }
+    <ImageBackground
+      source={{ uri: 'https://i.ibb.co/0t3nZGK/loginscreen-copy.jpg' }}
+      style={styles.background}
+      imageStyle={{
+        resizeMode: 'stretch',
+      }}
+    >
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        onSubmit={async (values) => {
+          // If user didn't input anything:
+          if (values.email === '' || values.password === '') {
+            Alert.alert('Error', 'Username and/or password cannot be empty.');
+          } else {
+            // If user did, axios call to lookup user login info
+            try {
+              let res = await axios.post(
+                'https://pocketbook-gh.herokuapp.com/auth/login/',
+                {
+                  email: values.email,
+                  password: values.password,
+                }
+              );
+              // handlePress = passes user info to function that will pass to RootNavigation's logIn function
+              handlePress({ user: res.data });
+              // If user info is invalid:
+            } catch {
+              Alert.alert(
+                'Error',
+                'Incorrect username or password. Please try again.'
+              );
             }
-          }}
-        >
-          {(props) => (
-            <View style={styles.container}>
-              {/* Icon Image */}
-              <Image
-                source={{
-                  uri: 'https://i.ibb.co/Gn9bqym/pocketbook-icon.png',
-                }}
-                style={styles.image}
+          }
+        }}
+      >
+        {(props) => (
+          <View>
+            {/* Icon Image */}
+            <Image
+              source={{
+                uri: 'https://i.ibb.co/Gn9bqym/pocketbook-icon.png',
+              }}
+              style={styles.image}
+            />
+            <Text style={styles.heading}>Login</Text>
+            <Text style={{ marginLeft: 20 }}>Welcome back to Pocketbook!</Text>
+
+            {/* Email Address Input */}
+            <View style={styles.inputContainer}>
+              <TextInput
+                label="Email"
+                selectionColor="#000"
+                style={styles.inputText}
+                theme={{ colors: { primary: '#000', placeholder: '#000' } }}
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+                onChangeText={props.handleChange('email')}
+                value={props.values.email}
+                left={<TextInput.Icon name="account-circle" />}
               />
-              <Text style={styles.heading}>Login</Text>
-              <Text>Welcome to Pocketbook!</Text>
+            </View>
 
-              {/* Email Address Input */}
-              <View style={styles.inputContainer}>
-                <TextInput
-                  label="Email"
-                  style={styles.inputText}
-                  theme={{ colors: { primary: '#000' } }}
-                  placeholder={'Email Address'}
-                  keyboardType="email-address"
-                  autoCapitalize="none"
-                  autoCorrect={false}
-                  onChangeText={props.handleChange('email')}
-                  value={props.values.email}
-                  left={<TextInput.Icon name="account-circle" />}
-                />
-              </View>
-
-              {/* Password Input */}
-              <View style={styles.inputContainer}>
-                <TextInput
-                  label="Password"
-                  style={styles.inputText}
-                  theme={{ colors: { primary: '#000' } }}
-                  secureTextEntry={secureTextEntry ? true : false}
-                  placeholder={'Password'}
-                  returnKeyType="go"
-                  onChangeText={props.handleChange('password')}
-                  value={props.values.password}
-                />
-                {/* Adds eye button that toggles whether password input is hidden or not */}
-                <TouchableOpacity
-                  style={styles.btnEye}
-                  onPress={updateSecureTextEntry}
-                >
-                  <Icon
-                    name={secureTextEntry ? 'eye-slash' : 'eye'}
-                    size={20}
-                    color={'grey'}
-                  />
-                </TouchableOpacity>
-              </View>
-
-              {/* Login Button */}
+            {/* Password Input */}
+            <View style={styles.inputContainer}>
+              <TextInput
+                label="Password"
+                style={styles.inputText}
+                theme={{ colors: { primary: '#000', placeholder: '#000' } }}
+                secureTextEntry={secureTextEntry ? true : false}
+                returnKeyType="go"
+                onChangeText={props.handleChange('password')}
+                value={props.values.password}
+                left={<TextInput.Icon name="lock" />}
+              />
+              {/* Adds eye button that toggles whether password input is hidden or not */}
               <TouchableOpacity
-                style={[styles.inputContainer, styles.submitContainer]}
-                onPress={props.handleSubmit}
+                style={styles.btnEye}
+                onPress={updateSecureTextEntry}
               >
-                <Text style={styles.submitText}>LOGIN</Text>
+                <Icon
+                  name={secureTextEntry ? 'eye' : 'eye-slash'}
+                  size={20}
+                  color={'#000'}
+                />
               </TouchableOpacity>
             </View>
-          )}
-        </Formik>
-        {/* Don't have an account? Navigates to Sign Up component */}
-        <View style={styles.navContainer}>
-          <Text>Don't have an account?</Text>
-          <Text
-            style={[{ color: 'blue' }, { marginLeft: 3 }]}
-            onPress={() =>
-              props.navigation.navigate('SignUp', { loggedIn: false })
-            }
-          >
-            Sign Up!
-          </Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+
+            {/* Login Button */}
+            <TouchableOpacity
+              style={[styles.inputContainer, styles.submitContainer]}
+              onPress={props.handleSubmit}
+            >
+              <Text style={styles.submitText}>LOGIN</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </Formik>
+      {/* Don't have an account? Navigates to Sign Up component */}
+      <View style={styles.navContainer}>
+        <Text>Don't have an account?</Text>
+        <Text
+          style={[{ color: 'blue' }, { marginLeft: 3 }]}
+          onPress={() =>
+            props.navigation.navigate('SignUp', { loggedIn: false })
+          }
+        >
+          Sign Up!
+        </Text>
+      </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginLeft: 30,
+    backgroundColor: '#FFF',
   },
   image: {
     width: 100,
     height: 100,
-    marginTop: 120,
+    marginTop: 90,
+    marginLeft: 10,
   },
   heading: {
+    marginLeft: 20,
     fontSize: 40,
     fontFamily: 'Asap-Bold',
   },
   // Edits the round container around the Input
   inputContainer: {
+    marginLeft: 20,
     marginTop: 20,
     // flexDirection: 'row',
-    // width: '95%',
+    width: '100%',
   },
   inputText: {
     fontWeight: 'bold',
     width: '90%',
+    backgroundColor: '#FFF',
   },
   submitContainer: {
     backgroundColor: '#6475a5',
@@ -195,8 +204,13 @@ const styles = StyleSheet.create({
   },
   btnEye: {
     position: 'absolute',
-    top: 12,
-    right: 20,
+    top: 25,
+    right: 40,
+    color: '#000',
+  },
+  background: {
+    width: '100%',
+    height: '100%',
   },
 });
 

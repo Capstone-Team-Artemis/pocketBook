f8e0cd563387deeda91504859d960620f2b00b14
Seonae <seonaebyeon1@gmail.com>
2021-04-05 00:05:33 -0400

    adds styling for text input and button

--- client/CreateEvent.js
+++ client/CreateEvent.js
@@ -1,20 +1,20 @@
 import {
   Text,
-  TextInput,
   StyleSheet,
   View,
   SafeAreaView,
   Dimensions,
   TouchableHighlight,
   TouchableOpacity,
-  Button,
   Alert,
+  ImageBackground,
+  Image,
 } from "react-native";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Icon from "react-native-vector-icons/FontAwesome";
 import RNDateTimePicker from "@react-native-community/datetimepicker";
-
+import { TextInput, Button } from 'react-native-paper'
 // import thunk
 import { postEvent } from "./store/events";
 
@@ -71,11 +71,19 @@ class CreateEvent extends Component {
   }
 
   render() {
-    const { eventTitle } = this.props.route.params;
 
     return (
       <SafeAreaView style={styles.container}>
+      <ImageBackground source={{ uri: 'https://i.ibb.co/3cP8BQC/booksignupscreen.jpg' }} style={styles.backgroundImg} >
+        
+        {/* navigation bar */}
         <View style={styles.navbar}>
+        <Image
+          source={{
+          uri: 'https://i.ibb.co/yWQvLJL/POCKETBOOK-1-IN-01.png',
+          }}
+          style={styles.image}
+        />
           <TouchableOpacity
             style={styles.navbar}
             style={{ alignItems: "flex-end", margin: 16 }}
@@ -84,14 +92,16 @@ class CreateEvent extends Component {
             <Icon name='bars' size={24} color='#161924' />
           </TouchableOpacity>
         </View>
-        <Text style={styles.heading}>{"Create Event"}</Text>
 
-        <View style={styles.inputContainer}>
-          {/* <GoogleAPI /> */}
+        <Text style={styles.heading}>Create Event</Text>
+
+        <View >
+
           <TextInput
             value={this.state.eventTitle}
             style={styles.input}
-            placeholder={"Title"}
+            label="Title"
+            multiline = {true}
             onChangeText={(eventTitle) => {
               this.setState({ eventTitle });
             }}
@@ -99,6 +109,7 @@ class CreateEvent extends Component {
 
           <RNDateTimePicker
             value={this.state.date}
+            type = {'outlined'}
             mode='date'
             display='default'
             onChange={(event, selectedDate) =>
@@ -132,55 +143,74 @@ class CreateEvent extends Component {
           <TextInput
             value={this.state.description}
             style={[styles.input, description]}
-            placeholder={"Description"}
+            multiline = {true}
+            error={true}
+            label="Description"
             onChangeText={(description) => {
               this.setState({ description });
             }}
           ></TextInput>
 
-          <TouchableHighlight
-            style={styles.button}
-            onPress={() => this.handleSubmit()}
+          <Button
+           mode="contained"
+           style={styles.button}
+           onPress={() => this.handleSubmit()}
           >
             <Text style={styles.submitText}>Submit</Text>
-          </TouchableHighlight>
+          </Button>
 
           <Button title='Go Back' onPress={() => this.handleGoBack()} />
         </View>
+        </ImageBackground>
       </SafeAreaView>
     );
   }
 }
 
-const description = { height: 155 };
+const description = { height: 170 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: "center",
-    justifyContent: "space-evenly",
+    justifyContent: "center",
   },
-  inputContainer: {
-    //backgroundColor:'#f0f8ff',
+  backgroundImg:{
+    width: '100%',
+    height: '100%',
+    resizeMode: 'stretch',
+  },
+  image: {
+    //marginTop: 10,
+    marginBottom: -50,
+    width: 180,
+    height: 100,
+  },
+  navbar: {
+    paddingLeft: 300,
   },
   heading: {
     fontSize: 40,
   },
   input: {
-    width: WIDTH - 55,
-    height: 35,
+    width: WIDTH - 45,
+    height: 55,
     marginTop: 20,
-    backgroundColor: "#f0f8ff",
-    borderRadius: 50,
-    borderWidth: 1.5,
+    backgroundColor: "#fff",
+    
   },
   button: {
-    alignItems: "center",
-    backgroundColor: "#6475a5",
-    padding: 10,
-    borderRadius: 45,
-    marginTop: 20,
-    borderWidth: 1.5,
+    // alignItems: "center",
+    // backgroundColor: "#6475a5",
+    // padding: 10,
+    // borderRadius: 45,
+    // marginTop: 20,
+    // borderWidth: 1.5,
+    width: 200,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#E92228',
+    marginTop: 30,
+    borderRadius: 350,
   },
   submitText: {
     color: "white",
@@ -188,9 +218,6 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     bottom: 2,
-  },
-  navbar: {
-    paddingLeft: 300,
   },
 });
 
